b25bf352e8116bc010fda62221de6cd84a05e8d2
Taeyoung20230727 <qksrm5546@gmail.com>
2024-07-23 17:10:36 +0900

    haha

---
+++ src/main/java/com/pokeguide/dto/TermsDTO.java
@@ -0,0 +1,18 @@
+package com.pokeguide.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class TermsDTO {
+
+    public String terms;
+    public String privacy;
+}
